c0a87f6b8516bf620c6103be4f22a3a533336842
13180 <w@VTSD.COM>
2022-09-14 09:27:14 -0400

    tg

---
+++ Lab02v100.java
@@ -0,0 +1,18 @@
+public class Lab02v100 {
+    public static void main(String args[]) {
+        System.out.println("Lab 02, 100 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge");
+        System.out.println("By yesterday's confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println();
+        System.out.println("#  ##### #####    ##### ##### #  #    #  #####  ####");
+        System.out.println("#  #   # #   #    #   # #   # #  ##   #    #    #");
+        System.out.println("#  #   # #   #    ##### #   # #  # #  #    #     #");
+        System.out.println("#  #   # #   #    #     #   # #  #   ##    #      #");
+        System.out.println("#  ##### #####    #     ##### #  #    #    #    ###");
+    }
+}
